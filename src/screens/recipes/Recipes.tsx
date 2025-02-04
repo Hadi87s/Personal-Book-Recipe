6ea7fb7be05f6cf573f338422ba9bffc60b7ac5c
Hadi Irshaid <HadiIrshaid8722@gmail.com>
2025-02-04 07:06:30 +0200

    fixing the grid logic.

--- src/screens/recipes/Recipes.tsx
+++ src/screens/recipes/Recipes.tsx
@@ -3,23 +3,59 @@ import { IRecipe } from "../../@Types";
 import Recipe from "../../components/recipe/Recipe";
 import "../screens.css";
 
-const recipe1: IRecipe = {
-  name: "Chicken Biryani",
-  image: "../../../public/recipe1.jpg",
-  description: "its very tasteful and I love it so much!",
-};
-const recipe2: IRecipe = {
-  name: "Chicken Serrano",
-  image: "../../../public/recipe2.jpg",
-  description: "its very delicious and I love it.",
-};
-
 const Recipes = () => {
-  const [recipes, setRecipes] = useState<IRecipe[]>([]);
+  const [recipes, setRecipes] = useState<IRecipe[]>([
+    {
+      name: "Chicken Biryani",
+      image: "../../../public/recipe1.jpg",
+      description: "its very tasteful and I love it so much!",
+    },
+    {
+      name: "Chicken Serrano",
+      image: "../../../public/recipe2.jpg",
+      description: "its very delicious and I love it.",
+    },
+    {
+      name: "Chicken Serrano",
+      image: "../../../public/recipe2.jpg",
+      description: "its very delicious and I love it.",
+    },
+    {
+      name: "Chicken Serrano",
+      image: "../../../public/recipe2.jpg",
+      description: "its very delicious and I love it.",
+    },
+    {
+      name: "Chicken Serrano",
+      image: "../../../public/recipe2.jpg",
+      description: "its very delicious and I love it.",
+    },
+    {
+      name: "Chicken Serrano",
+      image: "../../../public/recipe2.jpg",
+      description: "its very delicious and I love it.",
+    },
+    {
+      name: "Chicken Serrano",
+      image: "../../../public/recipe2.jpg",
+      description: "its very delicious and I love it.",
+    },
+    {
+      name: "Chicken Serrano",
+      image: "../../../public/recipe2.jpg",
+      description: "its very delicious and I love it.",
+    },
+    {
+      name: "Chicken Serrano",
+      image: "../../../public/recipe2.jpg",
+      description: "its very delicious and I love it.",
+    },
+  ]);
   return (
     <div className="recipeContainer">
-      <Recipe recipe={recipe1} />
-      <Recipe recipe={recipe2} />
+      {recipes.map((recipe, index) => (
+        <Recipe recipe={recipe} key={index} />
+      ))}
     </div>
   );
 };
