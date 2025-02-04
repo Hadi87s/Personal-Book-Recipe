2463a336d8b45979989e18f44862fe08e77407a4
Hadi Irshaid <HadiIrshaid8722@gmail.com>
2025-02-04 22:18:31 +0200

    buffing the design.

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -9,20 +9,21 @@ const Details = ({ recipe }: IProps) => {
     <div className="holder">
       <div className="hero">
         <div className="recipe-image">
+          <div className="cat">{recipe.category}</div>
           <img src={recipe.image} alt="" />
         </div>
         <div className="title">
           <h1>{recipe.title}</h1>
           <p>{recipe.description}</p>
+          <h3>Recipe Ingredients:</h3>
+          <ul className="ingredients">
+            {recipe.ingredients.map((item) => {
+              return <li className="listOfIngredients">{item}</li>;
+            })}
+          </ul>
         </div>
       </div>
       <div className="lists">
-        <ul className="ingredients">
-          <h3>Recipe Ingredients:</h3>
-          {recipe.ingredients.map((item) => {
-            return <li className="listOfIngredients">{item}</li>;
-          })}
-        </ul>
         <ol className="instructions">
           <h3>Recipe Instructions:</h3>
           {recipe.instructions.map((item) => {
