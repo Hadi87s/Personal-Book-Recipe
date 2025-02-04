d9d64ea5e5f0d6354a1ae738ddb5e06c1e7fc851
Hadi Irshaid <HadiIrshaid8722@gmail.com>
2025-02-04 22:09:20 +0200

    designing the skeleton of the details page for each recipe.

---
+++ src/components/Details/Details.tsx
@@ -0,0 +1,37 @@
+import { IRecipe } from "../../@Types";
+import "./details.css";
+interface IProps {
+  recipe: IRecipe;
+}
+
+const Details = ({ recipe }: IProps) => {
+  return (
+    <div className="holder">
+      <div className="hero">
+        <div className="recipe-image">
+          <img src={recipe.image} alt="" />
+        </div>
+        <div className="title">
+          <h1>{recipe.title}</h1>
+          <p>{recipe.description}</p>
+        </div>
+      </div>
+      <div className="lists">
+        <ul className="ingredients">
+          <h3>Recipe Ingredients:</h3>
+          {recipe.ingredients.map((item) => {
+            return <li className="listOfIngredients">{item}</li>;
+          })}
+        </ul>
+        <ol className="instructions">
+          <h3>Recipe Instructions:</h3>
+          {recipe.instructions.map((item) => {
+            return <li className="listOfInstructions">{item}</li>;
+          })}
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+export default Details;
--- src/screens/recipe-details/RecipeDetails.tsx
+++ src/screens/recipe-details/RecipeDetails.tsx
@@ -1,15 +1,16 @@
 import { useParams } from "react-router-dom";
 import { RecipesContext } from "../../providers/recipesProvider";
 import { useContext } from "react";
+import Details from "../../components/Details/Details";
 
 const RecipeDetails = () => {
   const { id } = useParams();
   const { recipes } = useContext(RecipesContext);
   return (
     <div>
-      RecipeDetails
-      <br />
-      {recipes.map((recipe) => (recipe.id === id ? recipe.description : null))} 
+      {recipes.map((recipe) =>
+        recipe.id === id ? <Details recipe={recipe} /> : null
+      )}
     </div> // filtering process...
   );
 };
