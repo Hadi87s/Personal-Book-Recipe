d40b588350cd07e479a3b19285c200a778399b14
Hadi Irshaid <HadiIrshaid8722@gmail.com>
2025-02-07 21:09:58 +0200

    rebuilding the project.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
 import "./navbar.css";
-import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 const Navbar = () => {
   const [scrolled, setScrolled] = useState<boolean>(false);
